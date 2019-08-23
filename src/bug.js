b189ea1c307bbd4cc4c91a7500f58f60858a9cfe
Andrey Artamonychev <me@andrey.wtf>
2019-08-23 13:45:51 +0300

    Initial commit

---
+++ src/bug.js
@@ -0,0 +1,4 @@
+export default () =>
+  async () =>
+    Promise.resolve();
+
---
+++ src/test.js
@@ -0,0 +1,8 @@
+import test from 'ava';
+
+import bug from '../src/bug';
+
+test('expect something', async t => {
+  bug()();
+  t.pass();
+});
