34a6ef6a244c641c0dc2d0104bd955a662a00f41
Andrey Artamonychev <me@andrey.wtf>
2019-08-23 15:14:56 +0300

    simplify bug

--- src/bug.js
+++ src/bug.js
@@ -1,3 +1,2 @@
-export default () =>
-  async () =>
+export default async () =>
     Promise.resolve();
--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ import test from 'ava';
 import bug from '../src/bug';
 
 test('expect something', async t => {
-  await bug()();
+  await bug();
   t.pass();
 });
